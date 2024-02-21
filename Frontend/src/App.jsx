56c777c6bd1e40aa1931a6496b53bb7bfad8387d
Akshay-vaghasiya <akshayvaghasiya3636@gmail.com>
2024-02-21 12:00:57 +0530

    update some files and configuration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,68 @@
+import { useEffect } from 'react'
+import Sidebar from './components/Sidebar'
 import './styles/App.css'
+import { initFlowbite } from 'flowbite'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LoginPage from './views/pages/LoginPage'
+import Dashboard from './views/pages/Dashboard'
+import { useLoginContext } from './context/LoginContext'
+import AddBook from './views/pages/AddBook'
+import ShowBooks from './views/pages/ShowBooks'
+import IssueBook from './views/pages/IssueBook'
+import IssuedBooks from './views/pages/IssuedBooks'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
 
+  useEffect(() => {
+    initFlowbite();
+  })
+
+  const { isLogin } = useLoginContext();
+
   return (
     <>
-       
+      <BrowserRouter>
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition:Bounce
+        />
+        {
+          isLogin &&
+          <>
+            <Navbar />
+            <Sidebar />
+          </>
+        }
+
+        <Routes>
+
+          <Route path='/' element={isLogin ? <Dashboard /> : <LoginPage />} />
+          <Route path='/dashboard' element={isLogin ? <Dashboard /> : <LoginPage />} />
+          <Route path='/addbook' element={isLogin ? <AddBook /> : <LoginPage />} />
+          <Route path='/showbooks' element={isLogin ? <ShowBooks /> : <LoginPage />} />
+          <Route path='/issuebook' element={isLogin ? <IssueBook /> : <LoginPage />} />
+          <Route path='/issuedbooks' element={isLogin ? <IssuedBooks /> : <LoginPage />} />
+          <Route path='*' element={isLogin ? <Dashboard />: <LoginPage />} />
+
+        </Routes>
+
+
+
+      </BrowserRouter>
+
     </>
   )
 }
